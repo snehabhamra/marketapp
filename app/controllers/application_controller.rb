729cf695b9876b50523073ab1c859c04214fff55
Sneha <81133168+snehabhamra@users.noreply.github.com>
2021-08-09 03:45:51 +1000

    created dashboard controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(update: [:first_name, :last_name, :url])
+        devise_parameter_sanitizer.permit(sign_up: [:first_name, :last_name])
+    end
 end
